01f04457cb810bb6dac008fe40448ecfa4f1eb40
sandeepnaidu <nainala_sandeep@yahoo.com>
2023-04-18 11:52:26 +0530

    Updated the script with the echo commands

--- catalogue.sh
+++ catalogue.sh
@@ -6,17 +6,22 @@ echo -e "\e[36m<<<<<<<<<<< Adding Application User>>>>>>>>>>>>>>>>\e[0m"
 useradd roboshop
 echo -e "\e[36m<<<<<<<<<<<<<<< Creating Application directory >>>>>>>>>>>>>>>>>>>>>>\e[0m"
 mkdir /app
+echo -e "\e[33m<<<<<<<<<< Downloading application content >>>>>>>>>>>>>>>>>\e[0m"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
 cd /app
+echo -e "\e[33m<<<<<<<<<<<< unzip app content >>>>>>>>>>>>>>>>>>>>>>\e[0m"
 unzip /tmp/catalogue.zip
-cd /app
+echo -e "\e[33m<<<<<<<<<<<<<<<< Installing NodeJS packages >>>>>>>>>>>>>>>>>>\e[0m "
 npm install
-
+echo -e "\e[33m<<<<<<<<<<<<<< copying catalogue systemd files  >>>>>>>>>>>>>>>>>>\e[0m"
 cp catalogue.service /etc/systemd/system/catalogue.service
+echo -e "\e[33m<<<<<<<<<<<<<< starting catalogue serices >>>>>>>>>>>>>>>>>>\e[0m"
 systemctl daemon-reload
 systemctl enable catalogue
 systemctl start catalogue
-
+echo -e "\e[33m<<<<<<<<<<<<<< copy MongoDB repo >>>>>>>>>>>>>>>>>>\e[0m"
 cp mongo.repo /etc/yum.repos.d/mongo.repo
+echo -e "\e[33m<<<<<<<<<<<<<< Installing MongoDB client  >>>>>>>>>>>>>>>>>>\e[0m"
 yum install mongodb-org-shell -y
+echo -e "\e[33m<<<<<<<<<<<<<< Load schema >>>>>>>>>>>>>>>>>>\e[0m"
 mongo --host mongodb-dev.devops71.cloud </app/schema/catalogue.js
