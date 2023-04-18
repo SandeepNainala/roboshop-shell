f9fd825d72c045b36f91d1dcbaee045e3177054f
sandeepnaidu <nainala_sandeep@yahoo.com>
2023-04-18 16:02:44 +0530

    code updated

--- redis.sh
+++ redis.sh
@@ -8,4 +8,4 @@ echo -e "\e[33m<<<<<<<<<< update redis.conf file >>>>>>>>>>>>\e[0m"
 sed -i -e 's|127.0.0.1|0.0.0.0' /etc/redis.conf /etc/redis/redis.conf
 echo -e "\e[33m<<<<<<<<<<< start redis server >>>>>>>>>>>>\e[0m"
 systemctl enable redis
-systemctl start redis
+systemctl restart redis
