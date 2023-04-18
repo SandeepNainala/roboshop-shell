d7af2916eeaa930a5e8a3f647b6b314bb563b641
sandeepnaidu <nainala_sandeep@yahoo.com>
2023-04-18 16:25:37 +0530

    updated the user server in conf file modified

--- user.sh
+++ user.sh
@@ -5,7 +5,8 @@ yum install nodejs -y
 echo -e "\e[36m<<<<<<<<<<< add App user >>>>>>>>>>>>>\e[0m"
 useradd roboshop
 echo -e "\e[36m<<<<<<<<<<< create App dir >>>>>>>>>>>>>\e[0m"
-mkdir app
+rm -rf /app
+mkdir /app
 echo -e "\e[36m<<<<<<<<<<< download app content >>>>>>>>>>>>>\e[0m"
 curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip
 cd /app
@@ -13,7 +14,7 @@ echo -e "\e[36m<<<<<<<<<<< unzip user content >>>>>>>>>>>>>\e[0m"
 unzip /tmp/user.zip
 echo -e "\e[36m<<<<<<<<<<< download package >>>>>>>>>>>>>\e[0m"
 npm install
-echo -e "\e[33m<<<<<<<<<<<<<< copying catalogue systemd files  >>>>>>>>>>>>>>>>>>\e[0m"
+echo -e "\e[33m<<<<<<<<<<<<<< copying user systemd files  >>>>>>>>>>>>>>>>>>\e[0m"
 cp /home/centos/roboshop-shell/user.service /etc/systemd/system/user.service
 echo -e "\e[33m<<<<<<<<<<<<<< start user services >>>>>>>>>>>>>>>>>>\e[0m"
 systemctl daemon-reload
