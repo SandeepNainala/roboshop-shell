8d6aa62fa3fb725d07788ec5e53f6d5a9cef1b9e
sandeepnaidu <nainala_sandeep@yahoo.com>
2023-04-18 16:13:08 +0530

    updated the user script with echo

--- user.sh
+++ user.sh
@@ -1,15 +1,21 @@
+echo -e "\e[36m<<<<<<<<<<< configure nodeJS repo >>>>>>>>>>>>>\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+echo -e "\e[36m<<<<<<<<<<< install nodeJS repo >>>>>>>>>>>>>\e[0m"
 yum install nodejs -y
+echo -e "\e[36m<<<<<<<<<<< add App user >>>>>>>>>>>>>\e[0m"
 useradd roboshop
+echo -e "\e[36m<<<<<<<<<<< create App dir >>>>>>>>>>>>>\e[0m"
 mkdir app
+echo -e "\e[36m<<<<<<<<<<< download app content >>>>>>>>>>>>>\e[0m"
 curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip
 cd /app
+echo -e "\e[36m<<<<<<<<<<< unzip user content >>>>>>>>>>>>>\e[0m"
 unzip /tmp/user.zip
-cd /app
+echo -e "\e[36m<<<<<<<<<<< download package >>>>>>>>>>>>>\e[0m"
 npm install
-
-cp user.service /etc/systemd/system/user.service
-
+echo -e "\e[33m<<<<<<<<<<<<<< copying catalogue systemd files  >>>>>>>>>>>>>>>>>>\e[0m"
+cp /home/centos/roboshop-shell/user.service /etc/systemd/system/user.service
+echo -e "\e[33m<<<<<<<<<<<<<< start user services >>>>>>>>>>>>>>>>>>\e[0m"
 systemctl daemon-reload
 systemctl enable user
-systemctl start user
+systemctl restart user
