c9966dd0a57829245794aeda6f2b0ced7ea96e61
sandeepnaidu <nainala_sandeep@yahoo.com>
2023-04-18 15:40:19 +0530

    adding path and the server of Mongo

--- catalogue.sh
+++ catalogue.sh
@@ -21,8 +21,8 @@ systemctl daemon-reload
 systemctl enable catalogue
 systemctl start catalogue
 echo -e "\e[33m<<<<<<<<<<<<<< copy MongoDB repo >>>>>>>>>>>>>>>>>>\e[0m"
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo
 echo -e "\e[33m<<<<<<<<<<<<<< Installing MongoDB client  >>>>>>>>>>>>>>>>>>\e[0m"
 yum install mongodb-org-shell -y
 echo -e "\e[33m<<<<<<<<<<<<<< Load schema >>>>>>>>>>>>>>>>>>\e[0m"
-mongo --host mongodb-dev.devops71.cloud </app/schema/catalogue.js
+mongo --host 172.31.19.96 </app/schema/catalogue.js
