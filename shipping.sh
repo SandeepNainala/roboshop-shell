755e12c0c62fdb38e9bce1bbf745e5ff91fe75bb
sandeepnaidu <nainala_sandeep@yahoo.com>
2023-04-25 17:26:06 +0530

    updating the script

--- shipping.sh
+++ shipping.sh
@@ -1,6 +1,7 @@
 source ${script_path}/common.sh
 script=$(realpath "$0")
 script_path=$(dirname "$script")
+mysql_root_password=$1
 
 echo -e "\e[36m<<<<<<<<<<< install maven packages >>>>>>>>>>>>>\e[0m"
 yum install maven -y
@@ -20,12 +21,12 @@ mv target/shipping-1.0.jar shipping.jar
 echo -e "\e[36m<<<<<<<<<<< systemd files copy >>>>>>>>>>>>>\e[0m"
 
 cp ${script_path}/shipping.service /etc/systemd/system/shipping.service
-echo -e "\e[36m<<<<<<<<<<< start shipping service  >>>>>>>>>>>>>\e[0m"
-systemctl daemon-reload
-systemctl enable shipping
 
 echo -e "\e[36m<<<<<<<<<<< install mysql>>>>>>>>>>>>>\e[0m"
 yum install mysql -y
 echo -e "\e[36m<<<<<<<<<<< start mysql schema >>>>>>>>>>>>>\e[0m"
-mysql -h mysql-dev.devops71.cloud -uroot -pRoboShop@1 < /app/schema/shipping.sql
+mysql -h mysql-dev.devops71.cloud -uroot -p${mysql_root_password} < /app/schema/shipping.sql
+echo -e "\e[36m<<<<<<<<<<< start shipping service  >>>>>>>>>>>>>\e[0m"
+systemctl daemon-reload
+systemctl enable shipping
 systemctl restart shipping
