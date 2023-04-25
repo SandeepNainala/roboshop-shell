477b64d0cd02e508f099b8fe5f4fbd1d39f8d40c
sandeepnaidu <nainala_sandeep@yahoo.com>
2023-04-25 16:52:43 +0530

    updating the script

--- shipping.sh
+++ shipping.sh
@@ -1,6 +1,6 @@
+source ${script_path}/common.sh
 script=$(realpath "$0")
 script_path=$(dirname "$script")
-source ${script_path}/common.sh
 
 echo -e "\e[36m<<<<<<<<<<< install maven packages >>>>>>>>>>>>>\e[0m"
 yum install maven -y
