8920b5aa6139bb0a0e4c60ddf31bcdd54799e4e2
sandeepanainala <nainala_sandeep@yahoo.com>
2025-04-11 15:54:11 +0530

    roboshop - creating ec2 instance for all the servers using shell

--- create-ec2-roboshop.sh
+++ create-ec2-roboshop.sh
@@ -11,5 +11,19 @@ for name in ${instance[@]}; do
   else
     instance_type = "t3.micro"
   fi
-  echo "Creating instance for: $name with the instance: $instance_type"
+  echo "Creating instance for: $name with instance type: $instance_type"
+      instance_id=$(aws ec2 run-instances --image-id ami-041e2ea9402c46c32 --instance-type $instance_type --security-group-ids sg-0fea5e49e962e81c9 --subnet-id subnet-09863c54177764565 --query 'Instances[0].InstanceId' --output text)
+      echo "Instance created for: $name"
+
+      aws ec2 create-tags --resources $instance_id --tags Key=Name,Value=$name
+
+      if [ $name == "web"]
+      then
+        aws ec2 wait instance-running --instance-ids $instance_id
+        public_ip=$(aws ec2 describe-instances --instance-ids $instance_id --query 'Reservations[0].Instances[0].[PublicIpAddress]' --output text)
+        ip_to_use=$public_ip
+      else
+        private_ip=$(aws ec2 describe-instances --instance-ids $instance_id --query 'Reservations[0].Instances[0].[PrivateIpAddress]' --output text)
+        ip_to_use=$private_ip
+      fi
 done
